1f633adfbf8ca83d75216eaeec9f7d972332a22d
concentrateddon <don@concentratedtech.com>
2014-02-16 09:01:56 -0800

    Bug fixed in EnhancedHTML2

--- HTML/2014Feb_HTMLReporting/HTML Code/WindowsPowerShell/Modules/EnhancedHTML2/EnhancedHTML2.psm1
+++ HTML/2014Feb_HTMLReporting/HTML Code/WindowsPowerShell/Modules/EnhancedHTML2/EnhancedHTML2.psm1
@@ -8,6 +8,7 @@ interactivity. Intended to be used with HTML fragments that are produced
 by ConvertTo-EnhancedHTMLFragment. This command does not accept pipeline
 input.
 
+
 .PARAMETER jQueryURI
 A Uniform Resource Indicator (URI) pointing to the location of the 
 jQuery script file. You can download jQuery from www.jquery.com; you should
@@ -16,10 +17,13 @@ that starts with http:// or https://. Alternately, you can also provide
 a file system path to the script file, although this may create security
 issues for the Web browser in some configurations.
 
+
 Tested with v1.8.2.
+
 
 Defaults to http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js, which
 will pull the file from Microsoft's ASP.NET Content Delivery Network.
+
 
 .PARAMETER jQueryDataTableURI
 A Uniform Resource Indicator (URI) pointing to the location of the 
@@ -29,19 +33,25 @@ that starts with http:// or https://. Alternately, you can also provide
 a file system path to the script file, although this may create security
 issues for the Web browser in some configurations.
 
+
 Tested with jQuery DataTable v1.9.4
+
 
 Defaults to http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.3/jquery.dataTables.min.js,
 which will pull the file from Microsoft's ASP.NET Content Delivery Network.
+
 
 .PARAMETER CssStyleSheet
 The CSS style sheet content - not a file name. If you have a CSS file,
 you can load it into this parameter as follows:
 
+
     -CSSStyleSheet (Get-Content MyCSSFile.css)
+
 
 Alternately, you may link to a Web server-hosted CSS file by using the
 -CssUri parameter.
+
 
 .PARAMETER CssUri
 A Uniform Resource Indicator (URI) to a Web server-hosted CSS file.
@@ -50,24 +60,31 @@ can still provide an embedded style sheet, which makes the resulting
 HTML page more standalone. To provide an embedded style sheet, use
 the -CSSStyleSheet parameter.
 
+
 .PARAMETER Title
 A plain-text title that will be displayed in the Web browser's window
 title bar. Note that not all browsers will display this.
+
 
 .PARAMETER PreContent
 Raw HTML to insert before all HTML fragments. Use this to specify a main
 title for the report:
 
+
     -PreContent "<H1>My HTML Report</H1>"
+
 
 .PARAMETER PostContent
 Raw HTML to insert after all HTML fragments. Use this to specify a 
 report footer:
 
+
     -PostContent "Created on $(Get-Date)"
+
 
 .PARAMETER HTMLFragments
 One or more HTML fragments, as produced by ConvertTo-EnhancedHTMLFragment.
+
 
     -HTMLFragments $part1,$part2,$part3
 .EXAMPLE
@@ -79,6 +96,7 @@ avaiable options. It relies on Internet connectivity to retrieve
 JavaScript from Microsoft's Content Delivery Network. This 
 example uses an embedded stylesheet, which is defined as a here-string
 at the top of the script.
+
 
 $computername = 'localhost'
 $path = 'c:\'
@@ -95,6 +113,7 @@ h1 {
 h2 {
     border-top:1px solid #666666;
 }
+
 
 th {
     font-weight:bold;
@@ -131,6 +150,7 @@ th {
 </style>
 "@
 
+
 function Get-InfoOS {
     [CmdletBinding()]
     param(
@@ -142,6 +162,7 @@ function Get-InfoOS {
                'OSBuild'=$os.buildnumber}
     New-Object -TypeName PSObject -Property $props
 }
+
 
 function Get-InfoCompSystem {
     [CmdletBinding()]
@@ -156,6 +177,7 @@ function Get-InfoCompSystem {
                'Cores'=$cs.numberoflogicalprocessors}
     New-Object -TypeName PSObject -Property $props
 }
+
 
 function Get-InfoBadService {
     [CmdletBinding()]
@@ -172,6 +194,7 @@ function Get-InfoBadService {
     }
 }
 
+
 function Get-InfoProc {
     [CmdletBinding()]
     param(
@@ -184,6 +207,7 @@ function Get-InfoProc {
         New-Object -TypeName PSObject -Property $props
     }
 }
+
 
 function Get-InfoNIC {
     [CmdletBinding()]
@@ -201,6 +225,7 @@ function Get-InfoNIC {
     }
 }
 
+
 function Get-InfoDisk {
     [CmdletBinding()]
     param(
@@ -217,6 +242,7 @@ function Get-InfoDisk {
     }
 }
 
+
 foreach ($computer in $computername) {
     try {
         $everything_ok = $true
@@ -227,18 +253,22 @@ foreach ($computer in $computername) {
         $everything_ok = $false
     }
 
+
     if ($everything_ok) {
         $filepath = Join-Path -Path $Path -ChildPath "$computer.html"
+
 
         $params = @{'As'='List';
                     'PreContent'='<h2>OS</h2>'}
         $html_os = Get-InfoOS -ComputerName $computer |
                    ConvertTo-EnhancedHTMLFragment @params 
 
+
         $params = @{'As'='List';
                     'PreContent'='<h2>Computer System</h2>'}
         $html_cs = Get-InfoCompSystem -ComputerName $computer |
                    ConvertTo-EnhancedHTMLFragment @params 
+
 
         $params = @{'As'='Table';
                     'PreContent'='<h2>&diams; Local Disks</h2>';
@@ -254,12 +284,16 @@ foreach ($computer in $computername) {
                    ConvertTo-EnhancedHTMLFragment @params
 
 
+
+
         $params = @{'As'='Table';
                     'PreContent'='<h2>&diams; Processes</h2>';
                     'MakeTableDynamic'=$true;
                     'TableCssClass'='grid'}
         $html_pr = Get-InfoProc -ComputerName $computer |
                    ConvertTo-EnhancedHTMLFragment @params 
+
+
 
 
         $params = @{'As'='Table';
@@ -271,6 +305,7 @@ foreach ($computer in $computername) {
         $html_sv = Get-InfoBadService -ComputerName $computer |
                    ConvertTo-EnhancedHTMLFragment @params 
 
+
         $params = @{'As'='Table';
                     'PreContent'='<h2>&diams; NICs</h2>';
                     'EvenRowCssClass'='even';
@@ -281,6 +316,8 @@ foreach ($computer in $computername) {
                    ConvertTo-EnhancedHTMLFragment @params
 
 
+
+
         $params = @{'CssStyleSheet'=$style;
                     'Title'="System Report for $computer";
                     'PreContent'="<h1>System Report for $computer</h1>";
@@ -288,8 +325,12 @@ foreach ($computer in $computername) {
         ConvertTo-EnhancedHTML @params |
         Out-File -FilePath $filepath
 
+
     }
 }
+
+
+
 
 
 
@@ -306,6 +347,7 @@ foreach ($computer in $computername) {
         [Parameter(Mandatory=$True)][string[]]$HTMLFragments
     )
 
+
     <#
         Add CSS style sheet. If provided in -CssUri, add a <link> element.
         If provided in -CssStyleSheet, embed in the <head> section.
@@ -317,8 +359,9 @@ foreach ($computer in $computername) {
         $stylesheet = "<link rel=`"stylesheet`" href=`"$CssUri`" type=`"text/css`" />"
     }
     if ($PSBoundParameters.ContainsKey('CssStyleSheet')) {
-        $stylesheet = $CssStyleSheet | Out-String
+        $stylesheet = "<style>$CssStyleSheet</style>" | Out-String
     }
+
 
     <#
         Create the HTML tags for the page title, and for
@@ -331,11 +374,13 @@ foreach ($computer in $computername) {
     }
     $script += "<script type=`"text/javascript`" src=`"$jQueryURI`"></script>`n<script type=`"text/javascript`" src=`"$jQueryDataTableURI`"></script>"
 
+
     <#
         Render supplied HTML fragments as one giant string
     #>
     Write-Verbose "Combining HTML fragments"
     $body = $HTMLFragments | Out-String
+
 
     <#
         If supplied, add pre- and post-content strings
@@ -345,8 +390,9 @@ foreach ($computer in $computername) {
         $body = "$PreContent`n$body"
     }
     if ($PSBoundParameters.ContainsKey('postcontent')) {
-        $body = "$PostContent`n$body"
+        $body = "$body`n$PostContent"
     }
+
 
     <#
         Add a final script that calls the datatable code
@@ -361,6 +407,7 @@ foreach ($computer in $computername) {
     $datatable += '} );'
     $datatable += "</script>"
 
+
     <#
         Datatables expect a <thead> section containing the
         table header row; ConvertTo-HTML doesn't produce that
@@ -369,6 +416,7 @@ foreach ($computer in $computername) {
     Write-Verbose "Fixing table HTML"
     $body = $body -replace '<tr><th>','<thead><tr><th>'
     $body = $body -replace '</th></tr>','</th></tr></thead>'
+
 
     <#
         Produce the final HTML. We've more or less hand-made
@@ -379,7 +427,9 @@ foreach ($computer in $computername) {
     ConvertTo-HTML -Head "$stylesheet`n$titletag`n$script`n$datatable" -Body $body  
     Write-Debug "Finished producing final HTML"
 
+
 }
+
 
 function ConvertTo-EnhancedHTMLFragment {
 <#
@@ -388,30 +438,38 @@ Creates an HTML fragment (much like ConvertTo-HTML with the -Fragment switch
 that includes CSS class names for table rows, CSS class and ID names for the
 table, and wraps the table in a <DIV> tag that has a CSS class and ID name.
 
+
 .PARAMETER InputObject
 The object to be converted to HTML. You cannot select properties using this
 command; precede this command with Select-Object if you need a subset of
 the objects' properties.
 
+
 .PARAMETER EvenRowCssClass
 The CSS class name applied to even-numbered <TR> tags. Optional, but if you
 use it you must also include -OddRowCssClass.
+
 
 .PARAMETER OddRowCssClass
 The CSS class name applied to odd-numbered <TR> tags. Optional, but if you 
 use it you must also include -EvenRowCssClass.
 
+
 .PARAMETER TableCssID
 Optional. The CSS ID name applied to the <TABLE> tag.
+
 
 .PARAMETER DivCssID
 Optional. The CSS ID name applied to the <DIV> tag which is wrapped around the table.
 
+
 .PARAMETER TableCssClass
 Optional. The CSS class name to apply to the <TABLE> tag.
 
+
 .PARAMETER DivCssClass
 Optional. The CSS class name to apply to the wrapping <DIV> tag.
+
 
 .PARAMETER As
 Must be 'List' or 'Table.' Defaults to Table. Actually produces an HTML
@@ -419,11 +477,13 @@ table either way; with Table the output is a grid-like display. With
 List the output is a two-column table with properties in the left column
 and values in the right column.
 
+
 .PARAMETER Properties
 A comma-separated list of properties to include in the HTML fragment.
 This can be * (which is the default) to include all properties of the
 piped-in object(s). In addition to property names, you can also use a
 hashtable similar to that used with Select-Object. For example:
+
 
  Get-Process | ConvertTo-EnhancedHTMLFragment -As Table `
                -Properties Name,ID,@{n='VM';
@@ -431,19 +491,23 @@ hashtable similar to that used with Select-Object. For example:
                                      css={if ($_.VM -gt 100) { 'red' }
                                           else { 'green' }}}
 
+
 This will create table cell rows with the calculated CSS class names.
 E.g., for a process with a VM greater than 100, you'd get:
+
 
   <TD class="red">475858</TD>
   
 You can use this feature to specify a CSS class for each table cell
 based upon the contents of that cell. Valid keys in the hashtable are:
 
+
   n, name, l, or label: The table column header
   e or expression: The table cell contents
   css or csslcass: The CSS class name to apply to the <TD> tag 
   
 Another example:
+
 
   @{n='Free(MB)';
     e={$_.FreeSpace / 1MB -as [int]};
@@ -457,17 +521,22 @@ cell will be given the CSS class "blue." The supplied cascading
 style sheet must define ".red" and ".blue" for those to have any
 effect.  
 
+
 .PARAMETER PreContent
 Raw HTML content to be placed before the wrapping <DIV> tag. 
 For example:
 
+
     -PreContent "<h2>Section A</h2>"
+
 
 .PARAMETER PostContent
 Raw HTML content to be placed after the wrapping <DIV> tag.
 For example:
 
+
     -PostContent "<hr />"
+
 
 .PARAMETER MakeHiddenSection
 Used in conjunction with -PreContent. Adding this switch, which
@@ -475,24 +544,30 @@ needs no value, turns your -PreContent into  clickable report
 section header. The section will be hidden by default, and clicking
 the header will toggle its visibility.
 
+
 When using this parameter, consider adding a symbol to your -PreContent
 that helps indicate this is an expandable section. For example:
 
+
     -PreContent '<h2>&diams; My Section</h2>'
+
 
 If you use -MakeHiddenSection, you MUST provide -PreContent also, or
 the hidden section will not have a section header and will not be
 visible.
+
 
 .PARAMETER MakeTableDynamic
 When using "-As Table", makes the table dynamic. Will be ignored
 if you use "-As List". Dynamic tables are sortable, searchable, and
 are paginated.
 
+
 You should not use even/odd styling with tables that are made
 dynamic. Dynamic tables automatically have their own even/odd
 styling. You can apply CSS classes named ".odd" and ".even" in 
 your CSS to style the even/odd in a dynamic table.
+
 
 .EXAMPLE
  $fragment = Get-WmiObject -Class Win32_LogicalDisk |
@@ -503,10 +578,12 @@ your CSS to style the even/odd in a dynamic table.
                                     -MakeHiddenSection `
                                     -MakeTableDynamic
 
+
  You will usually save fragments to a variable, so that multiple fragments
  (each in its own variable) can be passed to ConvertTo-EnhancedHTML.
 .NOTES
 Consider adding the following to your CSS when using dynamic tables:
+
 
     .paginate_enabled_next, .paginate_enabled_previous {
         cursor:pointer; 
@@ -526,15 +603,19 @@ Consider adding the following to your CSS when using dynamic tables:
     }
     .dataTables_info { margin-bottom:4px; }
 
+
 This applies appropriate coloring to the next/previous buttons,
 and applies a small amount of space after the dynamic table.
+
 
 If you choose to make sections hidden (meaning they can be shown
 and hidden by clicking on the section header), consider adding
 the following to your CSS:
 
+
     .sectionheader { cursor:pointer; }
     .sectionheader:hover { color:red; }
+
 
 This will apply a hover-over color, and change the cursor icon,
 to help visually indicate that the section can be toggled.
@@ -544,6 +625,7 @@ to help visually indicate that the section can be toggled.
         [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
         [object[]]$InputObject,
 
+
         [string]$EvenRowCssClass,
         [string]$OddRowCssClass,
         [string]$TableCssID,
@@ -551,16 +633,22 @@ to help visually indicate that the section can be toggled.
         [string]$DivCssClass,
         [string]$TableCssClass,
 
+
         [ValidateSet('List','Table')]
         [string]$As = 'Table',
 
+
         [object[]]$Properties = '*',
+
 
         [string]$PreContent,
 
+
         [switch]$MakeHiddenSection,
 
+
         [switch]$MakeTableDynamic,
+
 
         [string]$PostContent
     )
@@ -571,6 +659,7 @@ to help visually indicate that the section can be toggled.
             instead produce a single string.
         #>
         $out = ''
+
 
         <#
             Add the section header (pre-content). If asked to
@@ -592,6 +681,7 @@ to help visually indicate that the section can be toggled.
             }
         }
 
+
         <#
             The table will be wrapped in a <div> tag for styling
             purposes. Note that THIS, not the table per se, is what
@@ -605,7 +695,7 @@ to help visually indicate that the section can be toggled.
             $temp = ""
         }
         if ($PSBoundParameters.ContainsKey('MakeHiddenSection')) {
-            $temp += "id=`"$tempid`" style=`"display:none;`""
+            $temp += " id=`"$tempid`" style=`"display:none;`""
         } else {
             $tempid = ''
         }
@@ -613,6 +703,7 @@ to help visually indicate that the section can be toggled.
             $temp += " id=`"$DivCSSID`""
         }
         $out += "<div $temp>"
+
 
         <#
             Create the table header. If asked to make the table dynamic,
@@ -641,6 +732,7 @@ to help visually indicate that the section can be toggled.
         }
         $out += "<table $css>"
 
+
         <#
             We're now setting up to run through our input objects
             and create the table rows
@@ -649,19 +741,23 @@ to help visually indicate that the section can be toggled.
         $wrote_first_line = $false
         $even_row = $false
 
+
         if ($properties -eq '*') {
             $all_properties = $true
         } else {
             $all_properties = $false
         }
 
+
     }
     PROCESS {
+
 
         foreach ($object in $inputobject) {
             Write-Verbose "Processing object"
             $datarow = ''
             $headerrow = ''
+
 
             <#
                 Apply even/odd row class. Note that this will mess up the output
@@ -682,12 +778,14 @@ to help visually indicate that the section can be toggled.
                 Write-Verbose "No row CSS class"
             }
 
+
             <#
                 If asked to include all object properties, get them.
             #>
             if ($all_properties) {
                 $properties = $object | Get-Member -MemberType Properties | Select -ExpandProperty Name
             }
+
 
             <#
                 We either have a list of all properties, or a hashtable of
@@ -698,6 +796,7 @@ to help visually indicate that the section can be toggled.
                 $name = $null
                 $value = $null
                 $cell_css = ''
+
 
                 <#
                     $prop is a simple string if we are doing "all properties,"
@@ -718,6 +817,7 @@ to help visually indicate that the section can be toggled.
                     if ($prop.ContainsKey('cssclass')) { $cell_css = $Object | ForEach $prop['cssclass'] }
                     if ($prop.ContainsKey('css')) { $cell_css = $Object | ForEach $prop['css'] }
 
+
                     <#
                         Get the current property name.
                     #>
@@ -726,11 +826,13 @@ to help visually indicate that the section can be toggled.
                     if ($prop.ContainsKey('label')) { $name = $prop['label'] }
                     if ($prop.ContainsKey('l')) { $name = $prop['l'] }
 
+
                     <#
                         Execute the "expression" or "e" key to get the value of the property.
                     #>
                     if ($prop.ContainsKey('e')) { $value = $Object | ForEach $prop['e'] }
                     if ($prop.ContainsKey('expression')) { $value = $tObject | ForEach $prop['expression'] }
+
 
                     <#
                         Make sure we have a name and a value at this point.
@@ -745,6 +847,7 @@ to help visually indicate that the section can be toggled.
                     #>
                     Write-Warning "Unhandled property $prop"
                 }
+
 
                 <#
                     When constructing a table, we have to remember the
@@ -765,6 +868,7 @@ to help visually indicate that the section can be toggled.
                 }
             }
 
+
             <#
                 Write the table header, if we're doing a table.
             #>
@@ -773,6 +877,7 @@ to help visually indicate that the section can be toggled.
                 $out += "<tr>$headerrow</tr><tbody>"
                 $wrote_first_line = $true
             }
+
 
             <#
                 In table mode, write the data row.
@@ -790,10 +895,11 @@ to help visually indicate that the section can be toggled.
         #>
         Write-Verbose "PostContent"
         if ($PSBoundParameters.ContainsKey('PostContent')) {
-            $fragment = "$PostContent`n$fragment"
+            $fragment = "$fragment`n$PostContent"
         }
         Write-Verbose "Done"
         $out += "</tbody></table></div>"
         Write-Output $out
     }
 }
+
